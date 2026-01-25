0a451e4deb43f813650a175198319842b91fed7f
kaya <kaya_frog_ramer@kakao.com>
2026-01-25 15:55:11 +0900

    [BE] feature: TokenBlacklistService 구현

--- meari-be/src/main/java/com/ssafy/meari/global/service/TokenBlacklistService.java
+++ meari-be/src/main/java/com/ssafy/meari/global/service/TokenBlacklistService.java
@@ -1,16 +1,39 @@
 package com.ssafy.meari.global.service;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import com.ssafy.meari.global.jwt.JwtUtil;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class TokenBlacklistService {
-	// TODO redis로 만료된 토큰 관리하는 기능 구현 예정
+	private final RedisTemplate<String, String> redisTemplate;
+	private final JwtUtil jwtUtil;
 
-	public boolean isBlacklisted(String accessToken) {
-		return false; // TODO 구현 예정
+	// Access Token을 블랙리스트에 추가
+	public void addToBlacklist(String token) {
+		// 토큰의 남은 유효 시간 계산
+		long remainingTime = jwtUtil.getRemainingTime(token);
+
+		// 남은 시간이 0보다 클 때만 블랙리스트에 추가
+		if (remainingTime > 0) {
+			redisTemplate.opsForValue().set(
+				"blacklist:" + token,
+				"logout",
+				remainingTime,
+				TimeUnit.MILLISECONDS
+			);
+		}
+	}
+
+	// 블랙리스트 확인
+	public boolean isBlacklisted(String token) {
+		return redisTemplate.hasKey("blacklist:" + token);
 	}
 
 }
