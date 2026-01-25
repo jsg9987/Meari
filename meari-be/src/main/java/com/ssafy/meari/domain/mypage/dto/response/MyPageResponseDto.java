67307066850ef298dd84288b5f7135a5f5041d27
kaya <kaya_frog_ramer@kakao.com>
2026-01-25 23:20:19 +0900

    [BE] refactor: 마이페이지 응답 DTO 내 모국어 필드 타입 문자열로 변경
    기존에는 NativeLanguage enum 타입을 받았지만, enum 타입을 반환할 경우 VN, KR와 같은 영문 축약어가 반환되기 때문에 enum 대신 문자열을 받도록 하였다.

--- meari-be/src/main/java/com/ssafy/meari/domain/mypage/dto/response/MyPageResponseDto.java
+++ meari-be/src/main/java/com/ssafy/meari/domain/mypage/dto/response/MyPageResponseDto.java
@@ -1,7 +1,6 @@
 package com.ssafy.meari.domain.mypage.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.ssafy.meari.domain.member.entity.NativeLanguage;
 
 public record MyPageResponseDto(
 	String nickname,
@@ -9,6 +8,6 @@ public record MyPageResponseDto(
 	@JsonProperty("profile_image_url")
 	String profileImageUrl,
 	@JsonProperty("native_language")
-	NativeLanguage nativeLanguage
+	String nativeLanguage
 ) {
 }
