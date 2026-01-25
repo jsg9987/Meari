e551692021e5a12952f9ee5fb638f00bfeae6ce1
jedo5000 <jsg998877@gmail.com>
2026-01-26 02:08:39 +0900

    # [BE] feature(MemberRepository.java): MemberRepository 구현
    - 이메일로 회원 조회
    - 이메일 존재여부 확인
    - 닉네임 존재 여부 확인

---
+++ meari-be/src/main/java/com/ssafy/meari/domain/member/repository/MemberRepository.java
@@ -0,0 +1,20 @@
+package com.ssafy.meari.domain.member.repository;
+
+import com.ssafy.meari.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // 이메일로 회원 조회
+    Optional<Member> findByEmail(String email);
+
+    // 이메일 존재 여부 확인
+    boolean existsByEmail(String email);
+
+    // 닉네임 존재 여부 확인
+    boolean existsByNickname(String nickname);
+}
