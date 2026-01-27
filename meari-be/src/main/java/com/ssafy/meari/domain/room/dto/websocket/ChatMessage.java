83b80c0ef27a52ae9d9cb6ed225acf1739efc767
김희수 <answerme0622@daum.net>
2026-01-27 15:58:17 +0900

    [BE] feature: 프론트에서 받을 채팅 데이터 형태 정의

--- meari-be/src/main/java/com/ssafy/meari/domain/room/dto/websocket/ChatMessage.java
+++ meari-be/src/main/java/com/ssafy/meari/domain/room/dto/websocket/ChatMessage.java
@@ -2,15 +2,32 @@ package com.ssafy.meari.domain.room.dto.websocket;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Schema(description = "실시간 채팅 메시지")
 public class ChatMessage {
-    private Long memberId;
+
+    private Long senderId;
+
     private String nickname;
-    private String content;
-    private Long timestamp;
+
+    private String message;
+
+    private LocalDateTime timestamp;
+
+
+    public void setTimestampNow() {
+        this.timestamp = LocalDateTime.now();
+    }
 }
