359c9179555860a35106deb4f0fa4a6d5ee934af
kaya <kaya_frog_ramer@kakao.com>
2026-01-27 23:47:24 +0900

    [BE] chore: 테스트를 위한 제한 해제 코드 주석처리

--- meari-be/src/main/java/com/ssafy/meari/global/config/SecurityConfig.java
+++ meari-be/src/main/java/com/ssafy/meari/global/config/SecurityConfig.java
@@ -77,12 +77,12 @@ public class SecurityConfig {
                 .requestMatchers("/error").permitAll()
 
                 ////////////////// chat-test.html 테스트를 위한 접근제한 해제 /////////////
-               .requestMatchers("/**/*.html").permitAll()
-               .requestMatchers("/**/*.js").permitAll()
-               .requestMatchers("/**/*.css").permitAll()
-               .requestMatchers("/**/*.ico").permitAll()
-               .requestMatchers("/**/*.png").permitAll()
-               .requestMatchers("/**/*.jpg").permitAll()
+               // .requestMatchers("/**/*.html").permitAll()
+               // .requestMatchers("/**/*.js").permitAll()
+               // .requestMatchers("/**/*.css").permitAll()
+               // .requestMatchers("/**/*.ico").permitAll()
+               // .requestMatchers("/**/*.png").permitAll()
+               // .requestMatchers("/**/*.jpg").permitAll()
 
                 // 나머지는 인증 필요
                 .anyRequest().authenticated()
