24fbc9f6160a7f3b73b7a8bd05028d718373f158
전석균 <jsg998877@gmail.com>
2026-01-29 22:53:21 +0900

    Update file JwtAuthenticationFilter.java

--- meari-be/src/main/java/com/ssafy/meari/global/auth/jwt/JwtAuthenticationFilter.java
+++ meari-be/src/main/java/com/ssafy/meari/global/auth/jwt/JwtAuthenticationFilter.java
@@ -40,9 +40,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 "/api/v1/auth/email/check".equals(requestURI) ||
                 "/api/v1/auth/nickname/check".equals(requestURI) ||
                 "/api/v1/auth/refresh".equals(requestURI) ||
+                "/websocket-test.html".equals(requestURI) ||
                 requestURI.startsWith("/swagger-ui") ||    // swagger-ui 관련 모든 리소스
                 requestURI.startsWith("/v3/api-docs") ||   // OpenAPI3 스펙 경로
-                requestURI.startsWith("/api-docs")         // 기존 api-docs 경로
+                requestURI.startsWith("/api-docs") ||      // 기존 api-docs 경로
+                requestURI.startsWith("/ws")
         ////////////////// chat-test.html 테스트를 위한 접근제한 해제 /////////////
             //            || requestURI.startsWith("/api/v1/auth") ||
             //            requestURI.startsWith("/ws") ||
@@ -53,7 +55,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             //            requestURI.endsWith(".png") ||
             //            requestURI.endsWith(".jpg")
         ) {
-            log.debug("JwtAuthentication 스킵");
+            log.debug("JwtAuthentication 스킵: {}", requestURI);
             filterChain.doFilter(request, response);
             return;
         }
