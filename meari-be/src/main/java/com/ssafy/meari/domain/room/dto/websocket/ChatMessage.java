6101171e31d15d8dd8a1a315978cd49876efe595
kaya <kaya_frog_ramer@kakao.com>
2026-01-29 23:26:55 +0900

    [BE] refactor: 실시간 채팅에 RabbitMQ 사용하지 않도록 제거

--- meari-be/src/main/java/com/ssafy/meari/domain/room/dto/websocket/ChatMessage.java
+++ meari-be/src/main/java/com/ssafy/meari/domain/room/dto/websocket/ChatMessage.java
@@ -1,18 +1,16 @@
 package com.ssafy.meari.domain.room.dto.websocket;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
--- meari-be/src/main/java/com/ssafy/meari/global/config/WebSocketConfig.java
+++ meari-be/src/main/java/com/ssafy/meari/global/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.ssafy.meari.global.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -13,38 +12,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${spring.rabbitmq.host:localhost}")
-    private String rabbitmqHost;
-
-    @Value("${spring.rabbitmq.stomp.port:61613}")
-    private int rabbitmqStompPort;
-
-    @Value("${spring.rabbitmq.username:ssafy}")
-    private String rabbitmqUsername;
-
-    @Value("${spring.rabbitmq.password:ssafy}")
-    private String rabbitmqPassword;
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 클라이언트가 구독할 목적지 prefix
-        // RabbitMQ STOMP Broker Relay 사용
+        // SimpleBroker 사용 (인메모리 메시지 브로커)
         // /topic: 1:N 브로드캐스트
         // /queue: 1:1 메시지
-        registry.enableStompBrokerRelay("/topic", "/queue")
-                .setRelayHost(rabbitmqHost)
-                .setRelayPort(rabbitmqStompPort)
-                .setClientLogin(rabbitmqUsername)
-                .setClientPasscode(rabbitmqPassword)
-                .setSystemLogin(rabbitmqUsername)
-                .setSystemPasscode(rabbitmqPassword)
-                .setSystemHeartbeatSendInterval(10000)
-                .setSystemHeartbeatReceiveInterval(10000);
+        registry.enableSimpleBroker("/topic", "/queue");
 
         // 클라이언트가 메시지를 보낼 때 사용할 prefix
         registry.setApplicationDestinationPrefixes("/app");
 
-        log.info("RabbitMQ STOMP Broker Relay 설정 완료 - host: {}, port: {}", rabbitmqHost, rabbitmqStompPort);
+        log.info("SimpleBroker 설정 완료 - /topic, /queue");
     }
 
     @Override
