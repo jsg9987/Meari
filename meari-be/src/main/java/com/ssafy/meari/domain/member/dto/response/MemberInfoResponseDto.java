d7b2464ca150e12d0647833c3c3440c20f21954c
김희수 <answerme0622@daum.net>
2026-01-30 13:44:27 +0900

    [BE] refactor: getMyInfo 메서드 반환값에 memberId 추가

--- meari-be/src/main/java/com/ssafy/meari/domain/member/dto/response/MemberInfoResponseDto.java
+++ meari-be/src/main/java/com/ssafy/meari/domain/member/dto/response/MemberInfoResponseDto.java
@@ -3,6 +3,7 @@ package com.ssafy.meari.domain.member.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record MemberInfoResponseDto(
+	Long memberId,
 	String email,
 	@JsonProperty("profile_url")
 	String profileUrl,
