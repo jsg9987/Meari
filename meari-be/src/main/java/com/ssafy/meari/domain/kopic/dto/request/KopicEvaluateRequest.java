98d8dba8f25f35d870f3185b78ec2166a3d0f6bb
장상민 <jangsangmin0401@gmail.com>
2026-02-06 10:55:01 +0900

    [BE] fix: 코픽 분석 복구

--- meari-be/src/main/java/com/ssafy/meari/domain/kopic/dto/request/KopicEvaluateRequest.java
+++ meari-be/src/main/java/com/ssafy/meari/domain/kopic/dto/request/KopicEvaluateRequest.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Schema(description = "코픽 발화 분석 요청")
 public class KopicEvaluateRequest {
@@ -21,5 +25,9 @@ public class KopicEvaluateRequest {
     @NotNull(message = "코픽 문장 ID는 필수입니다.")
     @Schema(description = "코픽 문장 ID", example = "501")
     private Long kopicSentenceId;
+
+    @NotBlank(message = "음성 파일 URL은 필수입니다.")
+    @Schema(description = "S3 음성 파일 URL", example = "https://s3.ap-northeast-2.amazonaws.com/bucket/audio.webm")
+    private String audioUrl;
 
 }
