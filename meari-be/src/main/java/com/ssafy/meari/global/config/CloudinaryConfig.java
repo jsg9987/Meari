591a252d10a9af8e105555b48228b6a60976180b
김희수 <242569263+kayaaaaaaaaaaa@users.noreply.github.com>
2026-02-07 14:20:51 +0900

    [BE] 클라우드너리 api 키 application.yml로 관리하도록 변경

--- meari-be/src/main/java/com/ssafy/meari/global/config/CloudinaryConfig.java
+++ meari-be/src/main/java/com/ssafy/meari/global/config/CloudinaryConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.meari.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +10,21 @@ import com.cloudinary.utils.ObjectUtils;
 @Configuration
 public class CloudinaryConfig {
 
+	@Value("${cloudinary.cloud-name}")
+	private String cloudName;
+
+	@Value("${cloudinary.api-key}")
+	private String apiKey;
+
+	@Value("${cloudinary.api-secret}")
+	private String apiSecret;
+
 	@Bean
 	public Cloudinary cloudinary() {
 		return new Cloudinary(ObjectUtils.asMap(
-			"cloud_name", "meari",
-			"api_key", "254257143383711",
-			"api_secret", "f3EWs0ckzl0pprPloOGgZTPCs_U"
+			"cloud_name", cloudName,
+			"api_key", apiKey,
+			"api_secret", apiSecret
 		));
 	}
 }
